712886d4e9fbcfeb60b3ecd9a91811c96712d2c5
Tania-2006 <taniabykaluk@gmail.com>
2025-07-17 12:44:02 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,17 +2,18 @@ import re
 from typing import Generator, Callable
 
 def generator_numbers(text: str) -> Generator[float, None, None]:
-    pattern = r'(?<=\s)(\d+\.\d+|\d+)(?=\s)'
-    matches = re.finditer(pattern, text)
+    pattern = r' \d+\.\d+ | \d+ '
+    matches = re.findall(pattern, text)
     for match in matches:
-        yield float(match.group(1))
+        yield float(match.strip())
 
 def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
     return sum(func(text))
 
 if __name__ == "__main__":
     text = ("Загальний дохід працівника складається з декількох частин: "
-            "1000.01 як основний дохід, доповнений додатковими надходженнями "
-            "27.45 і 324.00 доларів.")
+            " 1000.01 як основний дохід, доповнений додатковими надходженнями "
+            " 27.45 і 324.00 доларів.")
     total_income = sum_profit(text, generator_numbers)
-    print(f"Загальний дохід: {total_income}")  # Виведе: Загальний дохід: 1351.46
+    print(f"Загальний дохід: {total_income}")  
+
